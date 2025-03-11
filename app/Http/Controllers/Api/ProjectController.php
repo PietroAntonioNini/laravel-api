9d77b3de34dc113f537296d45bb9aa25b394d989
Pietro Antonio Nini <p.nini@evin.it>
2025-03-11 18:08:00 +0100

    Add filtri per Type

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,15 +8,32 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $projects = Project::with(['technologies', 'type'])->paginate(12);
+      $query = Project::with(['technologies', 'type']);
 
+      // Filtra per categoria (tipo di progetto)
+      if ($request->has('filter') && $request->filter != '') {
+        $filter = strtolower($request->filter);
+        $query->whereHas('type', function($q) use ($filter) {
+            $q->whereRaw('LOWER(name) = ?', [$filter]);
+        });
+      }
 
-        return response()->json([
-            "success" => true,
-            "results" => $projects,
-        ]);
+      // Filtra per tecnologia (es. Angular, HTML, ecc.)
+      if ($request->has('technology') && $request->technology != '') {
+        $technology = $request->technology;
+        $query->whereHas('technologies', function($q) use ($technology) {
+            $q->where('type', $technology);
+        });
+      }
+
+      $projects = $query->paginate(12);
+
+      return response()->json([
+        "success" => true,
+        "results" => $projects,
+      ]);
     }
 
     public function show($slug)
